47444b4791d084379307648754111b36486e1777
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-15 09:35:34 +0400

    new tings added

--- calc.py
+++ calc.py
@@ -1,10 +1,15 @@
 num1 = int(input("enter a number: "))
 num2 = int(input("enter a number: "))
-print(f"{num1} + {num2} = {num1+num2}")
-print(f"{num1} - {num2} = {num1-num2}")
-print(f"{num1} * {num2} = {num1*num2}")
-print(f"{num1} / {num2} = {num1/num2}")
+operation = input("enter one of + - * / mod or pow: ")
+
+if operation == '+':
+    print(f"{num1} + {num2} = {num1+num2}")
+elif operation == '-':
+    print(f"{num1} - {num2} = {num1-num2}")
+elif operation == '*':
+    print(f"{num1} * {num2} = {num1*num2}")
+elif operation == '/':
+    print(f"{num1} / {num2} = {num1/num2}")
 print(f"{num1} ^ {num2} = {pow(num1,num2)}")
 print(f"abs {num1} = {abs(num1)}")
 print(f"abs {num2} = {abs(num2)}")
-# done
